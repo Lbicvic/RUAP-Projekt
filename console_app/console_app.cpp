e1149d1579649b311d5c8dff2ad5f6ba7f103556
Nikola <nstipic@etfos.hr>
2021-11-03 17:42:22 +0100

    drugi commit

--- console_app/console_app.cpp
+++ console_app/console_app.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od Leopold Bičvić\n";
+    std::cout << "Pozdrav iz aplikacije od Leopold Bičvić nema broja indeksa\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
